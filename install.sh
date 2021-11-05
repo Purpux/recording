e2f8df09064b4cefda134f78125d8a9cede80bf3
Purpux <icaliberdev@gmail.com>
2021-11-05 11:58:09 +0530

    modified to separated pulse version and alsa version

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-doas cp $1 /usr/local/bin/$1
-doas chown root:wheel /usr/local/bin/$1
-doas chmod 650 /usr/local/bin/$1
+cp $1 /usr/local/bin/$1
+chown root:wheel /usr/local/bin/$1
+chmod 650 /usr/local/bin/$1
